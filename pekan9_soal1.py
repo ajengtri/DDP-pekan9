22160b4ceb122a62a0c39cd78e36faa8a451445e
ajengtri <150763243+ajengtri@users.noreply.github.com>
2023-11-22 15:09:09 +0700

    Add files via upload

---
+++ pekan9_soal1.py
@@ -0,0 +1,10 @@
+#soal no 1
+def tampilkan_data_diri(nama, alamat, gender, umur, hobi):
+    print("Nama:", nama)
+    print("Alamat:", alamat)
+    print("Gender:", gender)
+    print("Umur:", umur)
+    print("Hobi:", hobi)
+
+# Contoh pemanggilan fungsi
+tampilkan_data_diri("Ajeng Trihandayani", "Bogor", "perempuan", 20, "Treveling")
---
+++ pekan9_soal3.py
@@ -0,0 +1,7 @@
+def bilangan_ganjil(batas):
+    for i in range(batas+1):
+        if i % 2 != 0:
+            print(i)
+
+
+bilangan_ganjil(100)
